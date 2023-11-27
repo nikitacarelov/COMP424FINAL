7ab42d3b5dcd670b90c2054363b99f2c268e924a
devFOE <deployment@futureofengagement.ca>
2023-11-27 15:40:58 -0500

    tried IDS, buggy

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -18,7 +18,7 @@ class StudentAgent(Agent):
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
- 
+        self.moves_tree = defaultdict(dict)
         self.dir_map = {
             "u": 0,
             "r": 1,
@@ -26,41 +26,54 @@ class StudentAgent(Agent):
             "l": 3,
         }
 
-    def computeAllMoves(self, player_pos, other_pos, max_step, chess_board):
-        # Moves (Up, Right, Down, Left)
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        moves_tree = defaultdict(set)
+    def computeAllMoves(self, player_pos, other_pos, max_step, chess_board, max_depth_tree):
         visited = set()
-        
-        def dfs(x, y, dir, depth):
+        #switch between whos turn we're computing
+
+        def dfs(x, y, dir, steps_taken):
             
-            if depth == max_step + 1:
-                return False
-            visited.add((x,y))
-            moves_tree[player_pos].add((x,y))
-                
-            for (r, c, dir) in [(x - 1, y, 0), (x + 1, y, 2), (x, y - 1, 3), (x, y + 1, 1)]:
-                if (r,c) not in visited and not chess_board[x, y, dir] and not other_pos == (r,c) and 0 <= r < len(chess_board) and 0 <= c < len(chess_board[0]):
-                    if dfs(r, c, dir, depth + 1):
-                        return True
-            return False
+            if max_depth_tree <= 0:
+                return
         
+            if steps_taken == max_step + 1:
+                return
+            visited.add((x, y))
+
+            # which direction can we add walls for each square?
+            for dir_wall in range(0, 4):
+                if not chess_board[x, y, dir_wall]:
+                    board = deepcopy(chess_board)
+                    board[x][y][dir] = True
+                    
+                    tmp = player_pos
+                    p1_pos = other_pos
+                    p2_pos = tmp
+                    self.moves_tree[player_pos].update({(x, y, dir_wall):{self.computeAllMoves(p1_pos, p2_pos, max_step, board, max_depth_tree -1)}})
+
+            for (r, c, dir) in [(x - 1, y, 0), (x + 1, y, 2), (x, y - 1, 3), (x, y + 1, 1)]:
+                if (r, c) not in visited and not chess_board[x, y, dir] and not other_pos == (r, c) and 0 <= r < len(chess_board) and 0 <= c < len(chess_board[0]):
+                    dfs(r, c, dir, steps_taken + 1)
+                   
         x, y = player_pos
         dfs(x, y, 0, 0)
-        print(visited)
-        print(moves_tree)
+        return 
         
-            
-    def decideMove(self, player_pos, other_pos, max_step, chess_board):
-        #first compute all moves for my_pos since its my turn
-        #then compute advsery mvoes
-        #then my
-        #then ad
-        
-        self.computeAllMoves(player_pos, other_pos, max_step, chess_board)
-        #run IDS and find best move
-  
     
+
+    def decideMove(self, player_pos, other_pos, max_step, chess_board):
+        # def IDS(self, max_depth, player_pos, other_pos):
+        #     for _ in range(max_depth):
+        #         self.computeAllMoves(player_pos, other_pos, max_step, chess_board)
+
+        # self.IDS(defaultdict(dict), 3, player_pos, other_pos)
+        self.computeAllMoves(player_pos, other_pos, max_step, chess_board, 1)
+
+        print(self.moves_tree)
+
+       # minimax score heuristic: number squares I have to move - number squares adversay has to move in
+
+       # run (a,b) pruning
+
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
         Implement the step function of your agent here.
@@ -77,18 +90,16 @@ class StudentAgent(Agent):
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
 
-        # Some simple code to help you with timing. Consider checking 
+        # Some simple code to help you with timing. Consider checking
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
-        
 
         start_time = time.time()
-        #first compute all moves for my_pos since its my turn
+        # first compute all moves for my_pos since its my turn
         self.decideMove(my_pos, adv_pos, max_step, chess_board)
-        
-        
+
         time_taken = time.time() - start_time
-        
+
         print("My AI's turn took ", time_taken, "seconds.")
 
         # dummy return
