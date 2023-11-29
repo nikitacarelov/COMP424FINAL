aa24f4fce77f0eecde09047259d70ce2594438b8
devFOE <deployment@futureofengagement.ca>
2023-11-28 19:51:13 -0500

    added logic to step

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -29,7 +29,7 @@ class StudentAgent(Agent):
         allMoves = []
         num_squares_available = 0
 
-        def dfs(x, y, dir, steps_taken):
+        def dfs(x, y, steps_taken):
             nonlocal num_squares_available
             if steps_taken == max_step:
                 return
@@ -39,23 +39,23 @@ class StudentAgent(Agent):
             for dir_wall in range(0, 4):
                 if not chess_board[x, y, dir_wall]:
                     board = deepcopy(chess_board)
-                    board[x][y][dir] = True
-                    allMoves.append((((x, y), dir), board))
+                    board[x][y][dir_wall] = True
+                    allMoves.append((((x, y), dir_wall), board))
                     num_squares_available += 1
 
             for (r, c, dir) in [(x - 1, y, 0), (x + 1, y, 2), (x, y - 1, 3), (x, y + 1, 1)]:
                 if (r, c) not in visited and not chess_board[x, y, dir] and not other_pos == (r, c) and 0 <= r < len(chess_board) and 0 <= c < len(chess_board[0]):
-                    dfs(r, c, dir, steps_taken + 1)
+                    dfs(r, c, steps_taken + 1)
 
         x, y = player_pos
-        dfs(x, y, 0, 0)
+        dfs(x, y,0)
 
         return (allMoves, num_squares_available)
 
-    def minimax(self, player_pos, dir, other_pos, max_step, chess_board, depth, max_player,
+    def minimax(self, player_pos, other_pos, max_step, chess_board, depth, max_player,
                 alpha, beta):
 
-        # minimax score heuristic: number squares I have to move - number squares adversay has to move in
+        # minimax score heuristic: number squares I have to move - number squares adversary has to move in
         # depth limited minimax w AB pruning
         if depth == 2:
             nums_squares_I_have_to_move = self.computeAllMoves(
@@ -63,56 +63,44 @@ class StudentAgent(Agent):
 
             nums_squares_adv_have_to_move = self.computeAllMoves(
                 other_pos, player_pos, max_step, chess_board)[1]
-            pos = player_pos if max_player else other_pos
-            return (nums_squares_I_have_to_move - nums_squares_adv_have_to_move, (pos, dir))
+
+            return nums_squares_I_have_to_move - nums_squares_adv_have_to_move
 
         if max_player:
-            best = (-1000, ((0, 0), 0))
+            best = -1000
 
             # compute all moves
             moves_list = self.computeAllMoves(
                 player_pos, other_pos, max_step, chess_board)[0]
-            for (((max_player_pos, dir), board)) in moves_list:
+            for ((max_player_pos, direction), board) in moves_list:
 
                 # call minimax on all children of node
-                minimax_res = self.minimax(max_player_pos, dir, other_pos, max_step,
+                val = self.minimax(max_player_pos, other_pos, max_step,
                                            board, depth + 1, False, alpha, beta)
-                val = minimax_res[0]
-                best = best if max(
-                    best[0], val) == best[0] else minimax_res
 
-                alpha = alpha if max(
-                    alpha[0], best[0]) == alpha[0] else minimax_res
-                # alpha = max(alpha, best)
+                best = max(best, val)
+                alpha = max(alpha, best)
 
                 # Alpha Beta Pruning
-                if beta[0] <= alpha[0]:
+                if beta <= alpha:
                     break
             return best
 
         else:
-            best = (1000, ((0, 0), 0))
+            best = 1000
 
-            # Recur for left and
-            # right children
             moves_list = self.computeAllMoves(
                 other_pos, player_pos, max_step, chess_board)[0]
 
-            for (((min_player_pos, dir), board)) in moves_list:
-                minimax_res = self.minimax(player_pos, dir, min_player_pos, max_step,
-                                           board, depth + 1, True, alpha, beta)
+            for ((min_player_pos, direction), board) in moves_list:
+                val = self.minimax(player_pos, min_player_pos, max_step,
+                                   board, depth + 1, True, alpha, beta)
 
-                val = minimax_res[0]
-
-                best = best if min(
-                    best[0], val) == best[0] else minimax_res
-
-                beta = beta if min(
-                    alpha[0], best[0]) == alpha[0] else minimax_res
-                # beta = min(beta, best)
+                best = min(best, val)
+                beta = min(alpha, best)
 
                 # Alpha Beta Pruning
-                if beta[0] <= alpha[0]:
+                if beta <= alpha:
                     break
 
         return best
@@ -136,18 +124,37 @@ class StudentAgent(Agent):
         # Some simple code to help you with timing. Consider checking
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
-
-        start_time = time.time()
         # first compute all moves for my_pos since its my turn
 
-        minimax_res = self.minimax(my_pos, 0, adv_pos, max_step, chess_board, 0, True,
-                                   (-1000, ((0, 0), 0)), (1000, ((0, 0), 0)))
-        print("result of minimax: ", minimax_res)
+        start_time = time.time()
+        depth = 0
+        alpha = -1000
+        beta = 1000
+        best_moves = []
+
+        moves_list = self.computeAllMoves(my_pos, adv_pos, max_step, chess_board)[0]
+
+        for ((max_player_pos, direction), board) in moves_list:
+            # call minimax on all children of node
+            val = self.minimax(max_player_pos, adv_pos, max_step,
+                               board, depth + 1, False, alpha, beta)
+            if val > alpha:
+                alpha = val
+                best_moves.clear()
+                best_moves.append((max_player_pos, direction))
+            elif val == alpha:
+                best_moves.append((max_player_pos, direction))
+
+
+
+
 
         time_taken = time.time() - start_time
 
         print("My AI's turn took ", time_taken, "seconds.")
 
         # dummy return
-        return minimax_res[1]
-        # return my_pos, self.dir_map["u"]
+        return best_moves[np.random.randint(0, len(best_moves))] if len(best_moves) > 1 else best_moves[0]
+
+
+
